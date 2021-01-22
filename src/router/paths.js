f439966daa80a6ad597a8af476b9cb9c21cc007e
Moamen Abdelwahed <m.daood@yellow.com.eg>
2021-01-22 15:54:13 +0200

    Login

---
+++ src/router/paths.js
@@ -0,0 +1,74 @@
+/**
+ * Define all of your application routes here
+ * for more information on routes, see the
+ * official documentation https://router.vuejs.org/en/
+ */
+import store from '../store'
+export default [
+  {
+    path: '*',
+    meta: {
+      name: '',
+      requiresAuth: true
+    },
+    redirect: {
+      path: '/dashboard'
+    }
+  },
+  // This  allows you to have pages apart of the app but no rendered inside the dash
+  {
+    path: '/',
+    meta: {
+      name: '',
+      requiresAuth: false
+    },
+    component: () =>
+      import(/* webpackChunkName: "routes" */ `@/views/LoginHome.vue`),
+    // redirect if already signed in
+    beforeEnter: (to, from, next) => {
+      if (store.getters.authorized) {
+        next('/dashboard')
+      } else {
+        next()
+      }
+    },
+    children: [
+      {
+        path: '',
+        component: () => import(`@/components/LoginForm.vue`)
+      }
+    ]
+  },
+  // add any extra routes that you want rendered in the dashboard as a child below. Change toolbar names here
+  {
+    path: '/dashboard',
+    meta: {
+      name: 'Dashboard View',
+      requiresAuth: true
+    },
+    component: () => import(`@/views/DashboardView.vue`),
+    children: [
+      {
+        path: '',
+        name: 'Dashboard',
+        component: () => import(`@/components/DashViews/Dashboard.vue`)
+      },
+      {
+        path: 'user-profile',
+        meta: {
+          name: 'User Profile',
+          requiresAuth: true
+        },
+        component: () => import(`@/components/DashViews/UserProfile.vue`)
+      },
+      {
+        path: 'notifications',
+        meta: {
+          name: 'Notifications',
+          requiresAuth: true
+        },
+        component: () => import(`@/components/DashViews/Notifications.vue`)
+      }
+    ]
+  }
+]
